74f1cceed9cb9dbed01cf1789f4ba7fa3a1d0a04
gunesevitan <gunesevitan@gmail.com>
2023-12-28 11:21:26 +0300

    some models

--- src/classification_models/torch_modules.py
+++ src/classification_models/torch_modules.py
@@ -1,4 +1,4 @@
 from torch.nn import *
 
-from image_models import TimmConvImageClassificationModel, CoaTClassificationModel
+from image_models import TimmConvImageClassificationModel, CoaTClassificationModel, MaxViTClassificationModel, PVTClassificationModel
 from mil_models import TimmMILClassificationModel
