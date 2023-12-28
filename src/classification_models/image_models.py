60251416992f3723f25dea445785b80ad8ec0c4a
gunesevitan <gunesevitan@gmail.com>
2023-12-28 11:19:49 +0300

    image models

--- src/classification_models/image_models.py
+++ src/classification_models/image_models.py
@@ -1,3 +1,4 @@
+import yaml
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -23,7 +24,6 @@ class TimmConvImageClassificationModel(nn.Module):
                 parameter.requires_grad = False
 
         input_features = self.backbone.get_classifier().in_features
-        self.backbone.classifier = nn.Identity()
 
         self.pooling_type = pooling_type
         self.dropout = nn.Dropout(dropout_rate) if dropout_rate > 0 else nn.Identity()
@@ -49,11 +49,11 @@ class TimmConvImageClassificationModel(nn.Module):
         return cancer_output
 
 
-class TimmTransformerImageClassificationModel(nn.Module):
+class CoaTClassificationModel(nn.Module):
 
     def __init__(self, model_name, pretrained, backbone_args, dropout_rate, freeze_parameters, head_args):
 
-        super(TimmTransformerImageClassificationModel, self).__init__()
+        super(CoaTClassificationModel, self).__init__()
 
         self.backbone = timm.create_model(
             model_name=model_name,
@@ -66,8 +66,8 @@ class TimmTransformerImageClassificationModel(nn.Module):
                 parameter.requires_grad = False
 
         input_features = self.backbone.get_classifier().in_features
-        self.backbone.head.drop = nn.Identity()
-        self.backbone.head.fc = nn.Identity()
+        self.backbone.head_drop = nn.Identity()
+        self.backbone.head = nn.Identity()
         self.dropout = nn.Dropout(dropout_rate) if dropout_rate > 0 else nn.Identity()
         self.head = ClassificationHead(input_dimensions=input_features, **head_args)
 
@@ -78,3 +78,97 @@ class TimmTransformerImageClassificationModel(nn.Module):
         output = self.head(x)
 
         return output
+
+
+class MaxViTClassificationModel(nn.Module):
+
+    def __init__(self, model_name, pretrained, backbone_args, dropout_rate, freeze_parameters, head_args):
+
+        super(MaxViTClassificationModel, self).__init__()
+
+        self.backbone = timm.create_model(
+            model_name=model_name,
+            pretrained=pretrained,
+            **backbone_args
+        )
+
+        if freeze_parameters:
+            for parameter in self.backbone.parameters():
+                parameter.requires_grad = False
+
+        input_features = self.backbone.get_classifier().in_features
+        self.backbone.head.drop.p = dropout_rate
+        self.backbone.head.fc = ClassificationHead(input_dimensions=input_features, **head_args)
+
+    def forward(self, x):
+
+        x = self.backbone(x)
+
+        return x
+
+
+class PVTClassificationModel(nn.Module):
+
+    def __init__(self, model_name, pretrained, backbone_args, dropout_rate, freeze_parameters, head_args):
+
+        super(PVTClassificationModel, self).__init__()
+
+        self.backbone = timm.create_model(
+            model_name=model_name,
+            pretrained=pretrained,
+            **backbone_args
+        )
+
+        if freeze_parameters:
+            for parameter in self.backbone.parameters():
+                parameter.requires_grad = False
+
+        input_features = self.backbone.get_classifier().in_features
+        self.backbone.head_drop.p = dropout_rate
+        self.backbone.head = ClassificationHead(input_dimensions=input_features, **head_args)
+
+    def forward(self, x):
+
+        x = self.backbone(x)
+
+        return x
+
+
+def load_classification_model(model_directory, model_file_names, device):
+
+    """
+    Load model and pretrained weights from the given model directory
+
+    Parameters
+    ----------
+    model_directory: pathlib.Path
+        Path of the model directory
+
+    model_file_names: list
+        List of names of the model weights files
+
+    device: torch.device
+        Location of the model
+
+    Returns
+    -------
+    model: dict
+        Dictionary of models with weights loaded
+
+    config: dict
+        Dictionary of configurations
+    """
+
+    config = yaml.load(open(model_directory / 'config.yaml', 'r'), Loader=yaml.FullLoader)
+    config['model']['model_args']['pretrained'] = False
+
+    models = {}
+
+    for model_file_name in model_file_names:
+        model = eval(config['model']['model_class'])(**config['model']['model_args'])
+        model.load_state_dict(torch.load(model_directory / model_file_name))
+        model.to(device)
+        model.eval()
+        models[model_file_name] = model
+
+    return models, config
